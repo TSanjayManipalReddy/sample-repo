abe37b7a84c4a3ee185287a2ac8a639f65a12e99
T.Sanjay Manipal Reddy <sanjaymanipalreddyt@gmail.com>
2026-06-11 06:15:26 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,21 +1,13 @@
-import java.util.Scanner;
+public class Calculator {
 
-public class calculator {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter first number: ");
-        double num1 = sc.nextDouble();
-
-        System.out.print("Enter second number: ");
-        double num2 = sc.nextDouble();
+        int num1 = 10;
+        int num2 = 5;
 
         System.out.println("Addition: " + (num1 + num2));
         System.out.println("Subtraction: " + (num1 - num2));
         System.out.println("Multiplication: " + (num1 * num2));
         System.out.println("Division: " + (num1 / num2));
-
-        sc.close();
     }
 }
